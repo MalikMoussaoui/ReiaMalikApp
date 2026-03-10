1ab0b0e2a6cb484ee57772f34e52cd6e77a78bfd
Chupa <amalik.moussaoui1710@gmail.com>
2026-03-10 14:51:03 +0100

    Initial commit

---
+++ ReiaMalikApp/Views/AddPokemonPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ReiaMalikApp.Views;
+
+public partial class AddPokemonPage : ContentPage
+{
+	public AddPokemonPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ReiaMalikApp/Views/PokedexPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ReiaMalikApp.Views;
+
+public partial class PokedexPage : ContentPage
+{
+	public PokedexPage()
+	{
+		InitializeComponent();
+	}
+}
